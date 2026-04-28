f02f1d83ae37d7f6117b7b26be10df45067d5ff5
AndreiCeaetchii <ceaetchiiandrei@gmail.com>
2026-04-28 22:28:28 +0300

    build settings page with theme controls and backup tools

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import CarDetailPage from './pages/CarDetail';
 import ExpensesPage from './pages/Expenses';
 import InsurancePage from './pages/Insurance';
 import CarteaVerdePage from './pages/CarteaVerde';
+import SettingsPage from './pages/Settings';
 import NotFoundPage from './pages/NotFound';
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
           <Route path="expenses" element={<ExpensesPage />} />
           <Route path="insurance" element={<InsurancePage />} />
           <Route path="cartea-verde" element={<CarteaVerdePage />} />
+          <Route path="settings" element={<SettingsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
